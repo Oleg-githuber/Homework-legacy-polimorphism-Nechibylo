3b89e11e37a7912cc50baac98ca1f117a6b8b79a
Oleg-githuber <oilminer@mail.ru>
2024-08-20 04:11:39 +0500

    1

---
+++ Task1/Task1.cpp
@@ -0,0 +1,73 @@
+﻿/*
+В этом задании вы построите первую простую иерархию классов для заданной предметной области.
+
+Предметная область выглядит следующим образом: есть сущность фигура. Фигуры бывают двух типов: треугольник и четырёхугольник. У фигуры есть количество сторон. Для неизвестной фигуры будем считать это количество равным нулю. У треугольника три стороны, у четырёхугольника четыре стороны. У любой фигуры мы хотим узнавать количество её сторон, но мы не должны быть способны изменить количество сторон для заданной фигуры извне.
+
+Задача: спроектировать и реализовать классы, описывающие предметную область. Продемонстрируйте их работу: создайте по одному экземпляру каждого класса и выведите информацию о количестве его сторон на консоль.
+
+Пример работы программы
+*/
+
+#include <iostream>
+#include <clocale>
+#include <string>
+
+// Базовый класс
+class Figure
+{
+private:
+
+    int sidesCount{};
+    std::string name{ "Фигура" };
+
+public:
+
+    Figure() = default;
+
+    Figure(int value, std::string str) : sidesCount{ value }, name{str}
+    {}
+
+    std::string toString()
+    {
+        return name + ": " + std::to_string(sidesCount);
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+
+    Triangle() : Triangle{3}
+    {}
+
+    Triangle(int value) : Figure{ value, "Треугольник"}
+    {}
+};
+
+class Quadrangle : public Figure
+{
+public:
+
+    Quadrangle() : Quadrangle(4)
+    {}
+
+    Quadrangle(int value) : Figure{ value, "Четырёхугольник" }
+    {}
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Figure figure;
+    Triangle triangle;
+    Quadrangle quadrangle;
+
+    std::cout << "Количество сторон:\n";
+    std::cout << figure.toString() << std::endl
+        << triangle.toString() << std::endl
+        << quadrangle.toString() << std::endl;
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
